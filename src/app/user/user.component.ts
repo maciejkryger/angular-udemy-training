f6f819478d249ab9929482cfcc7486d1c6db9d62
Maciej Kryger <maciej.kryger@noreply.com>
2026-02-17 10:39:10 +0100

    initial commit with lessons up to 33 input with signals

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,35 @@
+import { Component, computed, signal, Input, input } from '@angular/core';
+
+// import { DUMMY_USERS } from '../dummy-users';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  // selectedUser = signal(DUMMY_USERS[randomUserIndex]);
+  // imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+  // imagePath=computed(()=>{
+  //   return 'assets/users/' + this.avatar();
+  // });
+
+  get imagePath() {
+  //   return 'assets/users/' + this.selectedUser.avatar;
+    return 'assets/users/' + this.avatar;
+  }
+
+  onSelectUser() {
+    // const randomUserIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    // this.selectedUser = DUMMY_USERS[randomUserIndex];
+    // this.selectedUser.set(DUMMY_USERS[randomUserIndex]);
+  }
+}
