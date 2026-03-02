81ad6624544a8862ce12273c34c0e63d5d0bbc6b
Maciej Kryger <maciej.kryger@noreply.com>
2026-03-02 16:45:18 +0100

    lessons up to 39 - tasks component started with selected user name

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal, Input, input } from '@angular/core';
+import { Component, computed, signal, Input, input , Output, EventEmitter, output} from '@angular/core';
 
 // import { DUMMY_USERS } from '../dummy-users';
 
@@ -13,10 +13,15 @@ export class UserComponent {
   // selectedUser = signal(DUMMY_USERS[randomUserIndex]);
   // imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
 
+  @Input({required: true}) id!: string;
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
   // avatar = input.required<string>();
   // name = input.required<string>();
+
+  // @Output() select = new EventEmitter<string>();
+  select = output<string>();
+
 
   // imagePath=computed(()=>{
   //   return 'assets/users/' + this.avatar();
@@ -31,5 +36,6 @@ export class UserComponent {
     // const randomUserIndex = Math.floor(Math.random() * DUMMY_USERS.length);
     // this.selectedUser = DUMMY_USERS[randomUserIndex];
     // this.selectedUser.set(DUMMY_USERS[randomUserIndex]);
+    this.select.emit(this.id);
   }
 }
