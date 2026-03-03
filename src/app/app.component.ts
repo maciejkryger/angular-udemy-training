ee9151b5c518f2ad2e4a2f7751f93a7253247986
Maciej Kryger <maciej.kryger@noreply.com>
2026-03-03 12:36:26 +0100

    lessson up tp 44 - if, and old version of for and if syntax

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
 import { TasksComponent } from "./tasks/tasks.component";
@@ -7,13 +9,13 @@ import { DUMMY_USERS } from "./dummy-users";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId = 'u1';
+  selectedUserId?: string;
 
   get selectedUser(){
     return this.users.find((user) => user.id === this.selectedUserId);
